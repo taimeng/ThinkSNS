d77164904bd3cd6a6d23f4578f0445d5c752b556
huoda <huoda.china@163.com>
2014-09-09 22:17:57 +0800

    upload
    huoda

---
+++ ThinkSNS_3.1/data/iswaf/config.php
@@ -0,0 +1,18 @@
+<?php
+return array (
+  'iswaf_database' => '/Users/huoda/Sites/ThinkSNS_3.1//data/iswaf/',
+  'iswaf_connenct_key' => '391327be346caa56255e362f1e4fd008',
+  'iswaf_status' => 1,
+  'defences' => 
+  array (
+    'callback_xss' => 'On',
+    'upload' => 'On',
+    'inject' => 'On',
+    'filemode' => 'On',
+    'webshell' => 'On',
+    'server_args' => 'On',
+    'webserver' => 'On',
+    'hotfixs' => 'On',
+  ),
+);
+?>
